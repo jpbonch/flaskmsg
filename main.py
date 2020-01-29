111d2a43cd5865c04a21c9eba5ac9ebf85b29ca1
yacaruna <jpbonchristiano04@gmail.com>
2020-01-29 10:40:26 +0000

    forgot import statement

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 from flask_sqlalchemy import SQLAlchemy
 from better_profanity import profanity
+from flask_cors import CORS
 
 
 app = Flask(__name__)
