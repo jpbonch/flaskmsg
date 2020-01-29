a3abf8363058de0b747563471ab91e3e915029af
tricknac <jp.bonchristiano@bol.com.br>
2020-01-29 18:51:22 +0000

    added socketio CORS

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send
 from flask_sqlalchemy import SQLAlchemy
 from better_profanity import profanity
-from flask_cors import CORS
-
 
 app = Flask(__name__)
-CORS(app)
 app.config['SECRET_KEY'] = '3432j5njkbn//bnksfg'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
